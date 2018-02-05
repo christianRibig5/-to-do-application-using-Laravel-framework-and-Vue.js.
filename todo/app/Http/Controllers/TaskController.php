738bdd5b9d14b52fc71f07e32e46c0d3712d3823
emmanuel irozuru <irozuruchimezerem@hotmail.com>
2018-02-05 13:38:59 +0100

    updated version

--- todo/app/Http/Controllers/TaskController.php
+++ todo/app/Http/Controllers/TaskController.php
@@ -8,26 +8,20 @@ class TaskController extends Controller
 {
     public function index()
     {
-        return Task::where('archive', 0)
-                      ->orderBy('id', 'desc')->get();
+        return Task::where('archive', 0)->orderBy('id', 'desc')->get();
     }
     public function archived()
     {
-        return Task::where('archive', 1)
-                       ->orderBy('id', 'desc')->get();
+        return Task::where('archive', 1)->orderBy('id', 'desc')->get();
     }
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'body' => 'required|max:500'
-        ]);
+        $this->validate($request, ['body' => 'required|max:500' ]);
         return Task::create(['body' => request('body')]);
     }
     public function edit(Request $request)
     {
-        $this->validate($request, [
-            'body' => 'required|max:500'
-        ]);
+        $this->validate($request, ['body' => 'required|max:500']);
         $task = Task::findOrFail($request->id);
         $task->body = $request->body;
         $task->save();
